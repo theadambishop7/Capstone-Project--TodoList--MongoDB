397c4bc648260b1cf814bc636481e793fde062af
theadambishop7 <theadambishop@gmail.com>
2023-08-08 11:49:56 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,69 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", { name: "Personal", todoList: personalTodoList });
+});
+
+app.get("/work", (req, res) => {
+    res.render("index.ejs", { name: "Work", todoList: workTodoList });
+});
+
+app.post('/update-todo', (req, res) => {
+    var { todoId, completedStatus, listName } = req.body;
+    if (completedStatus === 'true') {
+        completedStatus = true;
+    } else {
+        completedStatus = false;
+    }
+    if(listName === "Personal") {
+        personalTodoList[todoId].completed = completedStatus;
+    } else if(listName === "Work") {
+        workTodoList[todoId].completed = completedStatus;
+    }
+    
+    res.json({ success: true });
+});
+
+app.post('/add-todo', (req, res) => {
+    var { listName, todoName } = req.body;
+    if(listName === "Personal") {
+        personalTodoList.push(new toDoItem(todoName, false));
+        res.json({ success: true, updatedList: personalTodoList });
+    } else if(listName === "Work") {
+        workTodoList.push(new toDoItem(todoName, false));
+        res.json({ success: true, updatedList: workTodoList });
+    }
+});
+
+app.post('/delete-todo', (req, res) => {
+    var { todoId, listName } = req.body;
+    if(listName === "Personal") {
+        personalTodoList.splice(todoId, 1);
+        res.json({ success: true, updatedList: personalTodoList });
+    } else if(listName === "Work") {
+        workTodoList.splice(todoId, 1);
+        res.json({ success: true, updatedList: workTodoList });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+function toDoItem(item, completed) {
+    this.item = item;
+    this.completed = completed;
+}
+
+var personalTodoList = [new toDoItem("Buy milk", false), new toDoItem("Buy eggs", true), new toDoItem("Buy bread", false)];
+var workTodoList = [new toDoItem("Do work", false), new toDoItem("Get Raise", false), new toDoItem("Quit", false)];
---
+++ public/function.js
@@ -0,0 +1,158 @@
+// Get the current path
+var currentPath = window.location.pathname;
+
+// Remove the active class from all nav-links
+$(".nav-link").removeClass("active");
+
+// Add the active class to the appropriate nav-link based on the path
+if(currentPath === "/") {
+    $(".nav-link[href='/']").addClass("active");
+} else if(currentPath === "/work") {
+    $(".nav-link[href='/work']").addClass("active");
+}
+
+// Function to set the 'completed' class based on checkbox state
+function checkCheckboxState(checkbox) {
+    const span = checkbox.siblings(".todo-name");
+    if(checkbox.is(":checked")) {
+        span.addClass("completed");
+    } else {
+        span.removeClass("completed");
+    }
+}
+
+//Detect Keypress Enter on input
+$(document).on('keypress', "input#todo-input", function(event) {
+    if(event.which === 13) {
+        $("button#add-todo").click();
+    }
+});
+
+// When hovering over a todo item, show the delete button
+$(document).on('mouseenter', 'div.todo-item', function() {
+    $(this).children("button").toggleClass("d-none");
+}).on('mouseleave', 'div.todo-item', function() {
+    $(this).children("button").toggleClass("d-none");
+});
+
+// When clicking the delete button, remove the todo item
+$(document).on('click', 'div.todo-item button', function() {
+    const todoId = $(this).data('id');
+    const listName = $("h2").text();
+
+    $.ajax({
+        url: '/delete-todo',
+        method: 'POST',
+        data: {
+
+            todoId: todoId,
+            listName: listName
+        },
+        success: function(response) {
+            if(response.success) {
+                console.log("Todo deleted successfully!");
+
+                // Clear current todo list
+                $(".todo-item").remove();
+
+                // Append the updated todo list
+                response.updatedList.forEach(function(todo, index) {
+                    const isChecked = todo.completed ? 'checked' : '';
+                    const todoDiv = `
+                        <div class="todo-item">
+                            <input type="checkbox" data-id="${index}" ${isChecked} class="todo-checkbox">
+                            <span class="todo-name px-3">${todo.item}</span>
+                            <button class="btn btn-close d-none" data-id="${index}"></button>
+                        </div>`;
+                    $(".list-items").append(todoDiv);
+                    checkCheckboxState($(`input[data-id='${index}']`));
+                });
+
+            } else {
+                console.error("Error deleting todo.");
+            }
+        },
+        error: function(error) {
+            console.error("An error occurred:", error);
+        }
+    });
+});
+
+
+// Set class on page load for all checkboxes
+$("input[type='checkbox']").each(function() {
+    checkCheckboxState($(this));
+});
+
+// Set class when checkbox state changes
+$(document).on('change', "input[type='checkbox']", function() {
+    checkCheckboxState($(this));
+    
+    // Send an AJAX request to the server
+    const todoId = $(this).data('id');
+    const completedStatus = $(this).is(":checked");
+    const listName = $("h2").text();
+
+    $.ajax({
+        url: '/update-todo',
+        method: 'POST',
+        data: {
+            todoId: todoId,
+            completedStatus: completedStatus,
+            listName: listName
+        },
+        success: function(response) {
+            if(response.success) {
+                console.log("Todo updated successfully!");
+            } else {
+                console.error("Error updating todo.");
+            }
+        },
+        error: function(error) {
+            console.error("An error occurred:", error);
+        }
+    });
+});
+
+$(document).on('click', "button#add-todo", function() {
+    const listName = $("h2").text();
+    const todoName = $("input#todo-input").val();
+
+    $.ajax({
+        url: '/add-todo',
+        method: 'POST',
+        data: {
+            listName: listName,
+            todoName: todoName,
+        },
+        success: function(response) {
+            if(response.success) {
+                console.log("Todo updated successfully!");
+
+                // Clear current todo list
+                $(".todo-item").remove();
+                $("input#todo-input").val("");
+
+                // Append the updated todo list
+                response.updatedList.forEach(function(todo, index) {
+                    const isChecked = todo.completed ? 'checked' : '';
+                    const todoDiv = `
+                        <div class="todo-item">
+                            <input type="checkbox" data-id="${index}" ${isChecked} class="todo-checkbox">
+                            <span class="todo-name px-3">${todo.item}</span>
+                            <button class="btn btn-close d-none" data-id="${index}"></button>
+                        </div>`;
+                    $(".list-items").append(todoDiv);
+                    checkCheckboxState($(`input[data-id='${index}']`));
+                });
+                
+
+            } else {
+                console.error("Error updating todo.");
+            }
+        },
+        error: function(error) {
+            console.error("An error occurred:", error);
+        }
+    });
+});
